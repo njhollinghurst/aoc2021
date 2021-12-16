87e3d777e831ea0fd92d1860a72fe29409fec902
Nick Hollinghurst <njhollinghurst@github.com>
2021-12-16 18:18:57 +0000

    Day 15 (again): Use std::set for priority queue. Remove old solution

--- solve15.cc
+++ solve15.cc
@@ -2,10 +2,17 @@
 #include <cctype>
 #include <climits>
 #include <vector>
+#include <set>
 #include <iostream>
 
 using namespace std;
 
+//////////////////////////////////////////////////////////////////////
+// Represent the grid
+//////////////////////////////////////////////////////////////////////
+
+// This enum and the "dir" field are not currently used.
+// They could be used for path finding or visualization.
 enum Direction {
   LEFT,
   RIGHT,
@@ -18,7 +25,7 @@ struct Pos {
   unsigned  enter_risk;
   unsigned  exit_cost;
   Direction dir;
-  bool visited; // for Dijkstra version, below
+  bool visited;
 };
 
 unsigned W = 0, H = 0;
@@ -45,79 +52,7 @@ void rd()
   }
 }
 
-bool pass(Direction dir)
-{
-  bool any = false;
-  unsigned y, x;
-
-  switch (dir) {
-  case LEFT:
-    for(y = 0; y < H; ++y) {
-      for(x = 1; x < W; ++x) {
-	if (grid[y][x-1].enter_risk + grid[y][x-1].exit_cost < grid[y][x].exit_cost) {
-	  grid[y][x].exit_cost = grid[y][x-1].enter_risk + grid[y][x-1].exit_cost;
-	  grid[y][x].dir = LEFT;
-	  any = true;
-	}
-      }
-    }
-    break;
-  case RIGHT:
-    for(y = 0; y < H; ++y) {
-      for(x = W-1; x-- > 0;) {
-	if (grid[y][x+1].enter_risk + grid[y][x+1].exit_cost < grid[y][x].exit_cost) {
-	  grid[y][x].exit_cost = grid[y][x+1].enter_risk + grid[y][x+1].exit_cost;
-	  grid[y][x].dir = RIGHT;
-	  any = true;
-	}
-      }
-    }
-    break;
-  case UP:
-    for(y = 1; y < H; ++y) {
-      for(x = 0; x < W; ++x) {
-	if (grid[y-1][x].enter_risk + grid[y-1][x].exit_cost < grid[y][x].exit_cost) {
-	  grid[y][x].exit_cost = grid[y-1][x].enter_risk + grid[y-1][x].exit_cost;
-	  grid[y][x].dir = UP;
-	  any = true;
-	}
-      }
-    }
-    break;
-  case DOWN:
-    for(y = H-1; y-- > 0;) {
-      for(x = 0; x < W; ++x) {
-	if (grid[y+1][x].enter_risk + grid[y+1][x].exit_cost < grid[y][x].exit_cost) {
-	  grid[y][x].exit_cost = grid[y+1][x].enter_risk + grid[y+1][x].exit_cost;
-	  grid[y][x].dir = DOWN;
-	  any = true;
-	}
-      }
-    }
-    break;
-  default:
-    assert(0);
-  }
-  return any;
-}
-
-void task1()
-{
-  grid[H-1][W-1].exit_cost = 0;
-
-  bool any;
-  do {
-    // It's unlikely but possible that we could have to go LEFT or UP.
-    // XXX does that imply that iteration is required? Not sure...
-    any = pass(RIGHT);
-    any |= pass(DOWN);
-    any |= pass(LEFT);
-    any |= pass(UP);
-  } while (any);
-
-  cout << "Total path cost: " << grid[0][0].exit_cost << endl;
-}
-
+// Make the grid 25 times larger and reset it
 void frob_for_task2()
 {
   for(unsigned y = 0; y < H; ++y) {
@@ -144,115 +79,62 @@ void frob_for_task2()
 
 
 //////////////////////////////////////////////////////////////////////
-// The above is fine for both tasks, taking about 250ms for task 2;
-// but the following may be faster for very large grids.
-//
-// Hmm. The STL has a "priority_queue" but it apparently has no
-// facility for re-ordering existing elements of the queue(?)
+// Solve using Dijkstra's algorithm
+// Now using std::set to implement a "priority queue", sorted by cost
 //////////////////////////////////////////////////////////////////////
 
 struct QEnt {
-  unsigned short y;
-  unsigned short x;
-  QEnt(unsigned yy, unsigned xx) : y(yy), x(xx) { }
-  unsigned cost() const { return grid[y][x].exit_cost; }
-  bool operator==(QEnt const &r) const { return y==r.y && x==r.x; }
-  bool operator!=(QEnt const &r) const { return y!=r.y || x!=r.x; }
+  unsigned x;
+  unsigned y;
+  unsigned cost;
+  QEnt(unsigned xx, unsigned yy, unsigned cc) : x(xx), y(yy), cost(cc) { }
+  bool operator<(QEnt const &r) const {
+    return cost < r.cost || (cost == r.cost &&
+			     (y < r.y || (y == r.y && x < r.x)));
+  }
+  bool operator==(QEnt const &r) const {
+    return cost == r.cost && y == r.y && x == r.x;
+  }
 };
-
-vector<QEnt> the_q;  // keep this vector in REVERSE-SORTED ORDER
-
-void q_check()
-{
-  for(unsigned i = 1; i < the_q.size(); ++i) {
-    assert(the_q[i-1] != the_q[i]);
-    assert(the_q[i-1].cost() >= the_q[i].cost());
-  }
-}
-
-void q_reinsert(unsigned y, unsigned x, unsigned old_cost, unsigned new_cost)
-{
-  QEnt e(y,x);
-
-  // find where it used to be
-  unsigned old_pos = 0;
-  unsigned hi = the_q.size();
-  while (hi > old_pos) {
-    unsigned p = (old_pos + hi) >> 1;
-    if (the_q[p] == e) {
-      old_pos = p;
-      break;
-    }
-    if (the_q[p].cost() > old_cost)
-      old_pos = p+1;
-    else
-      hi = p;
-  }
-  while (old_pos < the_q.size() && the_q[old_pos] != e && the_q[old_pos].cost() >= old_cost)
-    ++old_pos;
-
-  // find where it ought to go
-  unsigned new_pos = old_pos;
-  hi = the_q.size();
-  while (hi > new_pos) {
-    unsigned p = (new_pos + hi) >> 1;
-    if (the_q[p] != e && the_q[p].cost() > new_cost)
-      new_pos = p+1;
-    else
-      hi = p;
-  }
-
-  // if it was never found, just insert at new_pos
-  if (old_pos >= the_q.size() || the_q[old_pos] != e) {
-    the_q.insert(the_q.begin() + new_pos, e);
-    return;
-  }
-
-  if (old_pos+1 < new_pos) {
-    new_pos--;
-    for(; old_pos < new_pos; ++old_pos) the_q[old_pos] = the_q[old_pos+1];
-    the_q[new_pos] = e;
-  }
-
-  //q_check();
-}
-
 
 void dijkstra()
 {
+  set<QEnt> the_q;
+  the_q.emplace(W-1, H-1, 0);
   grid[H-1][W-1].exit_cost = 0;
 
-  the_q.clear();
-  the_q.emplace_back(H-1,W-1);
-
-  while (the_q.size() > 0) {
+  while (!the_q.empty()) {
 
     // get the lowest-cost candidate node
-    unsigned y = the_q.back().y;
-    unsigned x = the_q.back().x;
+    unsigned y = the_q.begin()->y;
+    unsigned x = the_q.begin()->x;
     unsigned c = grid[y][x].exit_cost + grid[y][x].enter_risk;
     assert(!grid[y][x].visited);
+    the_q.erase(the_q.begin());
     grid[y][x].visited = true;
-    the_q.pop_back();
 
     // consider neighbours
     for(int dy = -1; dy <= 1; dy+=2) {
       if (y+(unsigned)dy < H) {
 	if (!grid[y+dy][x].visited && c < grid[y+dy][x].exit_cost) {
-	  unsigned c0 = grid[y+dy][x].exit_cost;
+	  QEnt e(x, y+dy, grid[y+dy][x].exit_cost);
+	  the_q.erase(e);
+	  e.cost = c;
 	  grid[y+dy][x].exit_cost = c;
 	  grid[y+dy][x].dir = (dy < 0) ? DOWN : UP;
-	  q_reinsert(y+dy, x, c0, c);
+	  the_q.insert(e);
 	}
       }
     }
     for(int dx = -1; dx <= 1; dx+=2) {
       if (x+(unsigned)dx < W) {
 	if (!grid[y][x+dx].visited && c < grid[y][x+dx].exit_cost) {
-	  unsigned c0 = grid[y][x+dx].exit_cost;
+	  QEnt e(x+dx, y, grid[y][x+dx].exit_cost);
+	  the_q.erase(e);
+	  e.cost = c;
 	  grid[y][x+dx].exit_cost = c;
 	  grid[y][x+dx].dir = (dx < 0) ? RIGHT : LEFT;
-	  q_reinsert(y, x+dx, c0, c);
+	  the_q.insert(e);
 	}
       }
     }
@@ -265,12 +147,15 @@ void dijkstra()
 int main()
 {
   rd();
-  task1();
+  dijkstra();
   frob_for_task2();
+  dijkstra();
 #if 0
-    task1();
-#else
-    dijkstra();
+  frob_for_task2(); // task 3?
+  dijkstra();
+  frob_for_task2(); // task 4?
+  dijkstra();
 #endif
+
   return 0;
 }
