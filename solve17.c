bb55bcc14211941883c0889270ca4e67570a3761
Nick Hollinghurst <njhollinghurst@github.com>
2021-12-17 10:47:12 +0000

    Day 17, rather unsatisfactory brute force search with guessed upper bound

---
+++ solve17.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <assert.h>
+
+int target_xlo, target_xhi, target_ylo, target_yhi;
+
+void rd()
+{
+  int n = scanf("target area: x=%d..%d, y=%d..%d",
+		&target_xlo, &target_xhi,
+		&target_ylo, &target_yhi);
+  assert(n == 4);
+}
+
+#define VMAX 16384 // XXX guessed upper bound
+
+void task1()
+{
+  int ubest = 0, vbest = 0, hbest = 0;
+  int n = 0;
+
+  int bound_xlo = (target_xlo < 0) ? target_xlo : 0;
+  int bound_xhi = (target_xhi > 0) ? target_xhi : 0;
+
+  for(int u0 = bound_xlo; u0 <= bound_xhi; u0++) {
+    for(int v0 = (target_ylo < 0) ? target_ylo : 0; v0 < VMAX; v0++) {
+      int u = u0;
+      int v = v0;
+      int x = 0;
+      int y = 0;
+      int h = 0;
+      while (y >= target_ylo && bound_xlo <= x && x <= bound_xhi &&
+	     (u != 0 || (target_xlo <= x && x <= target_xhi))) {
+	x += u;
+	y += v;
+	if (u > 0) u--; else if (u < 0) u++;
+	v--;
+	if (y > h) h = y;
+	if (x >= target_xlo && x <= target_xhi && y >= target_ylo && y <= target_yhi) {
+	  // Success!
+	  ++n;
+	  if (h > hbest) {
+	    ubest = u0;
+	    vbest = v0;
+	    hbest = h;
+	  }
+	  break;
+	}
+      }
+      if (y > target_yhi || (u==0 && 0 <= x && x < target_xlo)) {
+	// if it went out of bounds with y too high,
+	// or stalled and fell short, stop increasing v0
+	break;
+      }
+    }
+  }
+
+  printf("Best velocity (%d,%d) reaches height %d\n", ubest, vbest, hbest);
+  printf("Number: %d\n", n);
+}
+
+int main()
+{
+  rd();
+  task1();
+  return 0;
+}
